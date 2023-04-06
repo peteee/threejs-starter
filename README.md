ca07a2067fc6b4db5dd405040931449216678a8b
Peter Oberlaender <pete@wayoutofline.net>
2023-04-06 18:31:36 -0400

    readme.md and main.js updates

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # threejs-starter
 A packaged bundle that loads modules from your own stack
+
+Uses three.js r151...
+
+<img width="100%" src="https://raw.githubusercontent.com/peteee/threejs-starter/main/screenshots/Screen%20Shot%202023-04-06%20at%2018.04.32.png">
+
+NOTE: threejs-demo_simple.html uses three.js directly without modules! -> Only simple geometries are possible...
--- js/main.js
+++ js/main.js
@@ -94,28 +94,6 @@ function onWindowResize() {
 }
 /////////////////////////////////////////////
 
-
-/////// INTERACTIVE //////////////////////
-// const raycaster = new THREE.Raycaster();
-// const pointer = new THREE.Vector2();
-
-// function onPointerMove( event ) {
-
-//     // calculate pointer position in normalized device coordinates
-//     // (-1 to +1) for both components
-
-//     pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-//     pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-
-// }
-
-
-
-
-
-/////////////////////////////////////////////////
-
-
 // ADD TO SCENE & RENDER EVERYTHING
 scene.add( line );
 scene.add( cube );
